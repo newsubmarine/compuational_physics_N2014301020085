65605082aed77d7d6ec7cfefb96b4aa22ef9cd48
newsubmarine <976144544@qq.com>
2016-09-25 23:23:30 +0800

    Add files via upload

---
+++ exercise_3,list1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 25 22:42:43 2016
+
+@author: Administrator
+"""
+
+import os
+import time
+for a in range(0,20):      
+   print (' '*a+'##               ###########           ######           ##           ## ')
+   print (' '*a+'##               ##                 ##        ##        ## #         ## ')
+   print (' '*a+'##               ##                ##          ##       ##   #       ## ')
+   print (' '*a+'##               ###########       ##          ##       ##     #     ## ')
+   print (' '*a+'##               ##                ##          ##       ##       #   ## ')          
+   print (' '*a+'##               ##                 ##        ##        ##         # ## ')
+   print (' '*a+'############     ###########          #######           ##           ## ')
+   time.sleep(0.2)
+   os.system('cls')
